d9e39f01c2988cde5d3af1122e1f3d08170b82ac
Accult <acculltt@gmail.com>
2023-07-06 15:36:23 +0300

    View task Commit

--- mySite/base/urls.py
+++ mySite/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Tasks
+from .views import Tasks, TaskDetail
 
 urlpatterns=[
-    path('', Tasks.as_view(), name='Tasks'),
+    path('', Tasks.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
--- mySite/base/views.py
+++ mySite/base/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class Tasks(ListView):
     model = Task
-    context_object_name = 'Tasks'
+    context_object_name = 'tasks'
     template_name = 'base/task_list.html'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
 
 

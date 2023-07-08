59f85d1ff1912cae9cdcf1f1b8825f38a3f6e3da
Accult <acculltt@gmail.com>
2023-07-08 15:48:34 +0300

    Full Description Page Add to description page information about execution status and also add button 'complete' for mark task like completed

--- mySite/base/urls.py
+++ mySite/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Tasks, TaskDetail
+from .views import Tasks, TaskDetail, CompleteTaskView
 
 urlpatterns=[
     path('', Tasks.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/<int:pk>/complete/', CompleteTaskView.as_view(), name='complete_task'),
+
 ]
--- mySite/base/views.py
+++ mySite/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
+from django.views import View
 from django.views.generic.detail import DetailView
 from .models import Task
 
@@ -13,6 +14,26 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+
+class CompleteTaskView(View):
+    def post(self, request, pk):
+        # Retrieve the task object
+        task = Task.objects.get(pk=pk)
+
+        # Update the task's completion status
+        task.complete = True
+        task.save()
+
+        # Redirect back to the task detail page
+        return redirect('task', pk=pk)
+
+
+
+
+
+
 
 
 

ec9fe632506694168d3b6cc6d3b610aa24fa4217
Accult <acculltt@gmail.com>
2023-07-10 15:15:39 +0300

    can create, read, update, delete, complete task

--- mySite/base/urls.py
+++ mySite/base/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import Tasks, TaskDetail, CompleteTaskView
+from .views import Tasks, TaskDetail, CompleteTaskView, EditTaskView, DeleteTaskView, CreateTaskView
 
-urlpatterns=[
+urlpatterns = [
     path('', Tasks.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/<int:pk>/complete/', CompleteTaskView.as_view(), name='complete_task'),
+    path('task/<int:pk>/edit/', EditTaskView.as_view(), name='edit_task'),
+    path('task/<int:pk>/delete/', DeleteTaskView.as_view(), name='confirm_delete'),
+    path('create/', CreateTaskView.as_view(), name='create_task'),
 
 ]
--- mySite/base/views.py
+++ mySite/base/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views import View
 from django.views.generic.detail import DetailView
 from .models import Task
+
 
 class Tasks(ListView):
     model = Task
@@ -16,24 +17,61 @@ class TaskDetail(DetailView):
     template_name = 'base/task.html'
 
 
-
 class CompleteTaskView(View):
     def post(self, request, pk):
-        # Retrieve the task object
         task = Task.objects.get(pk=pk)
 
-        # Update the task's completion status
         task.complete = True
         task.save()
 
-        # Redirect back to the task detail page
         return redirect('task', pk=pk)
 
 
+class EditTaskView(View):
+    def get(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        return render(request, 'base/edit_task.html', {'task': task})
+
+    def post(self, request, pk):
+        task = Task.objects.get(pk=pk)
+
+        title = request.POST.get('title')
+        complete = request.POST.get('complete')
+        description = request.POST.get('description')
+
+        task.title = title
+        task.complete = complete == 'on'
+        task.description = description
+
+        task.save()
+
+        return redirect('tasks')
 
 
+class DeleteTaskView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        return render(request, 'base/confirm_delete.html', {'task': task})
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return redirect('tasks')
 
 
+class CreateTaskView(View):
+    def get(self, request):
+        return render(request, 'base/create_task.html')
 
+    def post(self, request):
+        title = request.POST.get('title')
+        complete = request.POST.get('complete', False) == 'on'
+        description = request.POST.get('description')
 
+        task = Task.objects.create(
+            title=title,
+            description=description,
+            complete=complete
+        )
 
+        return redirect('tasks')
